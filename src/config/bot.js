0f38024b5f636831361274866f214719e31b09fd
fitube1 <fitube1234@gmail.com>
2026-06-16 21:24:24 -0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "gatinho obeso e gordo, fa do pe do sonic",
         // Activity type number (0 = Playing).
         type: 0,
       },
